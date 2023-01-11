e726493778ee3b488cca2b3724195eff234b82ef
dinarazafy <dinasly01@gmail.com>
2023-01-11 09:14:30 +0300

    Initial commit , project creation

---
+++ src/main/java/com/cloud/projetcloudenchere/ProjetCloudEnchereApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.projetcloudenchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCloudEnchereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCloudEnchereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/projetcloudenchere/ProjetCloudEnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.projetcloudenchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCloudEnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
