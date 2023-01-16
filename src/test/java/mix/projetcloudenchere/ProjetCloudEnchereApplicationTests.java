b0678f2057e498876c2fe1b639c83b3e9603114d
dinarazafy <dinasly01@gmail.com>
2023-01-16 14:54:46 +0300

    Initial commit

---
+++ src/test/java/mix/projetcloudenchere/ProjetCloudEnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package mix.projetcloudenchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCloudEnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
