f5e02e987884dd92e2daf7d889092af8fb3bf8cf
dinarazafy <dinasly01@gmail.com>
2023-01-17 00:22:12 +0300

    ListeEncheres

--- src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
+++ src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
@@ -3,11 +3,13 @@ package mix.projetcloudenchere.controllerWeb;
 import mix.projetcloudenchere.model.Enchere;
 import mix.projetcloudenchere.repository.EnchereRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,7 +20,17 @@ public class EnchereController {
 
     @GetMapping("/encheres")
     public ResponseEntity<List<Enchere>> listeToutesEnchere(){
-        return  null;
+        try {
+            List<Enchere> enchere = new ArrayList<>();
+            enchereRepository.findAll().forEach(enchere::add);
+            if (enchere.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(enchere, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
