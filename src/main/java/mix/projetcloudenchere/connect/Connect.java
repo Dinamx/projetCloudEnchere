9b34922d29957cba44b33b633c3f46f4f437aac0
dinarazafy <dinasly01@gmail.com>
2023-01-16 23:17:02 +0300

    Connection

---
+++ src/main/java/mix/projetcloudenchere/connect/Connect.java
@@ -0,0 +1,22 @@
+package mix.projetcloudenchere.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Connect {
+    static String url="jdbc:postgresql://localhost:5432/encherecloud";
+    static String user="postgres";
+    static String password="root";
+
+    public static Connection connectToDatabase(){
+        try{
+            Connection c = null;
+            Statement stmt ;
+            c = DriverManager.getConnection(url,user,password);
+            return c;
+        }catch(Exception e){
+            return null;
+        }
+    }
+}
