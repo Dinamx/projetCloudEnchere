e239df76ca18f86221a131c9710e6fb2ebc8e2e2
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-01-16 21:08:06 +0300

    Mise a jour database

--- src/main/java/mix/projetcloudenchere/BaseEloic.sql
+++ src/main/java/mix/projetcloudenchere/BaseEloic.sql
@@ -15,14 +15,7 @@ drop table Produit;
 drop table categorie;
 drop table utilisateur;
 
--- Supprimer toutes les tables
-DO $$ DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
+
 
 -- 1
 Create table utilisateur(
