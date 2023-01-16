1cbf286c51763c8294a9f91e11e7f59a4d6915ee
dinarazafy <dinasly01@gmail.com>
2023-01-16 18:13:25 +0300

    Manao insertionProduit sy ValiderTransaction zao sisa

---
+++ src/main/java/mix/projetcloudenchere/model/Categorieproduit.java
@@ -0,0 +1,31 @@
+package mix.projetcloudenchere.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorieproduit")
+public class Categorieproduit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcategorieproduit", nullable = false)
+    private Integer id;
+
+    @Column(name = "categorie", length = 20)
+    private String categorie;
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
