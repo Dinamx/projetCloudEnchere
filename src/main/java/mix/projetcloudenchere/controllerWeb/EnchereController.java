b41dd332be0d651b098b3a3300217fbfff814899
dinarazafy <dinasly01@gmail.com>
2023-01-17 00:09:15 +0300

    Enchere repository Enchere controller +1

--- src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
+++ src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
@@ -1,9 +1,14 @@
 package mix.projetcloudenchere.controllerWeb;
 
+import mix.projetcloudenchere.repository.EnchereRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin
 public class EnchereController {
+    @Autowired
+    EnchereRepository enchereRepository;
+
 }
