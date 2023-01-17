5e642f459c861bc0298c3d3a573647ae1dce0550
dinarazafy <dinasly01@gmail.com>
2023-01-17 05:16:23 +0300

    ListeEnchereDetaille

--- src/main/java/mix/projetcloudenchere/repository/VueEnchereProduitUtilisateurRepository.java
+++ src/main/java/mix/projetcloudenchere/repository/VueEnchereProduitUtilisateurRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VueEnchereProduitUtilisateurRepository extends JpaRepository<VueEnchereProduitUtilisateur, Integer> {
-    List<VueEnchereProduitUtilisateur> findByIdutilisateur(String idUser);
+    List<VueEnchereProduitUtilisateur> findAllByIdutilisateur(int idUser);
 }
