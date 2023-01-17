06200df9cb587167e7a13f333ade01240fbbcd84
dinarazafy <dinasly01@gmail.com>
2023-01-17 05:16:23 +0300

    ListeEnchereDetaille

--- src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
+++ src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
@@ -2,12 +2,12 @@ package mix.projetcloudenchere.controllerWeb;
 
 import mix.projetcloudenchere.model.Enchere;
 import mix.projetcloudenchere.repository.EnchereRepository;
+import mix.projetcloudenchere.repository.VueEnchereProduitUtilisateurRepository;
+import mix.projetcloudenchere.views.VueEnchereProduitUtilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +17,18 @@ import java.util.List;
 public class EnchereController {
     @Autowired
     EnchereRepository enchereRepository;
+    @Autowired
+    VueEnchereProduitUtilisateurRepository vueEnchereProduitUtilisateurRepository;
+
+
 
     @GetMapping("/encheres")
-    public ResponseEntity<List<Enchere>> listeToutesEnchere(){
+    public ResponseEntity<List<VueEnchereProduitUtilisateur>> listeToutesEnchereDetails(){
         try {
             System.out.println("user");
 
-            List<Enchere> enchere = new ArrayList<>();
-            enchereRepository.findAll().forEach(enchere::add);
+            List<VueEnchereProduitUtilisateur> enchere = new ArrayList<>();
+            vueEnchereProduitUtilisateurRepository.findAll().forEach(enchere::add);
             if (enchere.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -35,6 +39,28 @@ public class EnchereController {
             return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/encheres/{iduser}")
+    public ResponseEntity<List<VueEnchereProduitUtilisateur>> allAuctionsByIdUser(@PathVariable String iduser){
+        try {
+            System.out.println("iduser" + iduser);
+            System.out.println("user");
+
+            List<VueEnchereProduitUtilisateur> enchere = new ArrayList<>();
+            vueEnchereProduitUtilisateurRepository.findAllByIdutilisateur(Integer.valueOf(iduser)).forEach(enchere::add);
+            if (enchere.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            System.out.println("retour");
+            return new ResponseEntity<>(enchere, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    @PostMapping("encherir")
+//    public
 
 
 }
--- src/main/java/mix/projetcloudenchere/model/Enchere.java
+++ src/main/java/mix/projetcloudenchere/model/Enchere.java
@@ -15,12 +15,12 @@ public class Enchere {
 
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
+//    @JsonIgnore
     @JoinColumn(name = "idutilisateur")
     private Utilisateur idutilisateur;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JsonIgnore
     @JoinColumn(name = "idproduit", nullable = false)
     private Produit idproduit;
 
