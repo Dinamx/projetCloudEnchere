437984112a2b20da529a4ba9bfad471b6ee8cac7
dinarazafy <dinasly01@gmail.com>
2023-01-17 08:24:03 +0300

    Push before change TODO ELOIC : Recherche Avancee

--- src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
+++ src/main/java/mix/projetcloudenchere/controllerWeb/EnchereController.java
@@ -1,7 +1,9 @@
 package mix.projetcloudenchere.controllerWeb;
 
 import mix.projetcloudenchere.model.Enchere;
+import mix.projetcloudenchere.model.Surenchere;
 import mix.projetcloudenchere.repository.EnchereRepository;
+import mix.projetcloudenchere.repository.SurenchereRepository;
 import mix.projetcloudenchere.repository.VueEnchereProduitUtilisateurRepository;
 import mix.projetcloudenchere.views.VueEnchereProduitUtilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,13 @@ import java.util.List;
 @CrossOrigin
 public class EnchereController {
     @Autowired
+    SurenchereRepository surenchereRepository;
+
+    @Autowired
     EnchereRepository enchereRepository;
     @Autowired
     VueEnchereProduitUtilisateurRepository vueEnchereProduitUtilisateurRepository;
+
 
 
 
@@ -29,6 +35,24 @@ public class EnchereController {
 
             List<VueEnchereProduitUtilisateur> enchere = new ArrayList<>();
             vueEnchereProduitUtilisateurRepository.findAll().forEach(enchere::add);
+            if (enchere.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            System.out.println("retour");
+            return new ResponseEntity<>(enchere, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/encheresList")
+    public ResponseEntity<List<Enchere>> toutesEnchereDetails(){
+        try {
+            System.out.println("user");
+
+            List<Enchere> enchere = new ArrayList<>();
+            enchereRepository.findAll().forEach(enchere::add);
             if (enchere.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -59,8 +83,24 @@ public class EnchereController {
         }
     }
 
-//    @PostMapping("encherir")
-//    public
+    @GetMapping("/encheres/history/{iduser}")
+    public ResponseEntity<List<Enchere>> getHistoryByUser(@PathVariable String iduser){
+        try {
+            System.out.println("iduser" + iduser);
+            System.out.println("user");
+
+            List<Enchere> enchere = new ArrayList<>();
+            enchereRepository.findAllByIdutilisateur(Integer.valueOf(iduser)).forEach(enchere::add);
+            if (enchere.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            System.out.println("retour");
+            return new ResponseEntity<>(enchere, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
 }
