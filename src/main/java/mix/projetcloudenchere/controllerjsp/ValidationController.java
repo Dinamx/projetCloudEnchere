9623f346b2258710175273e282abeb350ef80d6f
dinarazafy <dinasly01@gmail.com>
2023-01-17 11:24:01 +0300

    Avant Rechargementcompte

---
+++ src/main/java/mix/projetcloudenchere/controllerjsp/ValidationController.java
@@ -0,0 +1,44 @@
+package mix.projetcloudenchere.controllerjsp;
+
+import mix.projetcloudenchere.model.Rechargementcompte;
+import mix.projetcloudenchere.repository.RechargementcompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.persistence.EntityManager;
+
+@Controller
+public class ValidationController {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    RechargementcompteRepository rechargementcompteRepository;
+
+    @GetMapping("/valider/{idRechargement}")
+    public String validation(@PathVariable String idRechargement)
+    {
+        try{
+//            Initialisation listeRechargementEnAttente
+//            List<Rechargementcompte>
+
+            em.getTransaction().begin();
+
+//             updating stuffs
+            em.getTransaction().commit();
+            return "ok";
+        }
+        catch(Exception e){
+        em.getTransaction().rollback();
+        return "errorPage";
+    }
+        finally{
+        em.close();
+    }
+}
+
+
+}
