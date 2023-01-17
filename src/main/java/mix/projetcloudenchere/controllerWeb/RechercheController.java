5dbd7080e44f9908788319aeb2b01476b45eedee
dinarazafy <dinasly01@gmail.com>
2023-01-17 08:24:03 +0300

    Push before change TODO ELOIC : Recherche Avancee

---
+++ src/main/java/mix/projetcloudenchere/controllerWeb/RechercheController.java
@@ -0,0 +1,10 @@
+package mix.projetcloudenchere.controllerWeb;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+
+public class RechercheController {
+}
