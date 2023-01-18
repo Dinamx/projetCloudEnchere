590c2042ba8714590d474ebcecaa134a3a070f4f
dinarazafy <dinasly01@gmail.com>
2023-01-18 08:51:29 +0300

    Vue rechargement details non valides

--- src/main/java/mix/projetcloudenchere/controllerjsp/ValidationController.java
+++ src/main/java/mix/projetcloudenchere/controllerjsp/ValidationController.java
@@ -2,13 +2,16 @@ package mix.projetcloudenchere.controllerjsp;
 
 import mix.projetcloudenchere.model.Rechargementcompte;
 import mix.projetcloudenchere.repository.RechargementcompteRepository;
+import mix.projetcloudenchere.viewsRepository.VuedetailrechargementnonvalideRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Controller
 public class ValidationController {
@@ -17,6 +20,14 @@ public class ValidationController {
 
     @Autowired
     RechargementcompteRepository rechargementcompteRepository;
+
+    @Autowired
+    VuedetailrechargementnonvalideRepository nonvalide;
+
+    @GetMapping("listeRechargement")
+    public String listeRechargement(Model model){
+        model.addAttribute("nonvalide" ,nonvalide.findAll())
+    }
 
     @GetMapping("/valider/{idRechargement}")
     public String validation(@PathVariable int idRechargement)
---
+++ src/main/java/mix/projetcloudenchere/viewsRepository/VuedetailrechargementnonvalideRepository.java
@@ -0,0 +1,9 @@
+package mix.projetcloudenchere.viewsRepository;
+
+import mix.projetcloudenchere.views.Vuedetailrechargementnonvalide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VuedetailrechargementnonvalideRepository extends JpaRepository<Vuedetailrechargementnonvalide, Integer> {
+
+
+}
