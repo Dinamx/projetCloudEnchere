c2e8bf1a7669c61b4d59f474dad86646998daa02
dinarazafy <dinasly01@gmail.com>
2023-01-18 10:04:37 +0300

    Details enchere

--- src/main/java/mix/projetcloudenchere/views.sql
+++ src/main/java/mix/projetcloudenchere/views.sql
@@ -11,3 +11,6 @@ inner join categorieproduit c on c.idcategorieproduit = produit.idcategorieprodu
 create or replace view vuedetailrechargementNonValides as
     select r.idrechargementcompte,r.montant,r.dateheurechargement,u.idutilisateur,u.nom,u.prenom,u.email,u.dateinscription from rechargementcompte r join utilisateur u on u.idutilisateur = r.idutilisateur where r.validation = 0;
 
+create or replace  view detail_enchere as
+select e.idenchere,e.prixminimumvente,e.description as descriptionEnchere,u.idutilisateur,u.nom,u.prenom,p.description,p.idcategorieproduit,p.photo,p.nomproduit,e.dateheureenchere,e.dateheureenchere + e.dureeenchere * INTERVAL '1 minute' as dateFin from enchere e join utilisateur u on e.idutilisateur = u.idutilisateur join produit p on p.idproduit = e.idproduit;
+
