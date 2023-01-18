6b766b8e1e703c3b7205f8cae9f9289d06d493d0
dinarazafy <dinasly01@gmail.com>
2023-01-18 08:44:18 +0300

    Page d'eurreur
    errorPage.jsp

--- src/main/java/mix/projetcloudenchere/repository/RechargementcompteRepository.java
+++ src/main/java/mix/projetcloudenchere/repository/RechargementcompteRepository.java
@@ -2,6 +2,16 @@ package mix.projetcloudenchere.repository;
 
 import mix.projetcloudenchere.model.Rechargementcompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RechargementcompteRepository extends JpaRepository<Rechargementcompte, Integer> {
+
+//    TODO TEST
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE public.rechargementcompte SET validation = 1 WHERE idrechargementcompte =:idRechargement",nativeQuery = true)
+    public void updateRechargement(@Param("idRechargement")int idRechargement);
 }
