6cb8601d3203f839c4ea013658c9588b5c19a01c
dinarazafy <dinasly01@gmail.com>
2023-01-18 09:27:42 +0300

    Vita ny momba ny tena hoe Rechargement Compte , anao ny zavatra hafa tsika zao
    Rencherir

--- src/main/java/mix/projetcloudenchere/ProjetCloudEnchereApplication.java
+++ src/main/java/mix/projetcloudenchere/ProjetCloudEnchereApplication.java
@@ -3,8 +3,10 @@ package mix.projetcloudenchere;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @ComponentScan(basePackages = "mix.projetcloudenchere.controllerjsp")
 @ComponentScan(basePackages = "mix.projetcloudenchere.controllerWeb")
 public class ProjetCloudEnchereApplication {
