40458a63c5358c34f0105e4817e7fed802919a3f
dinarazafy <dinasly01@gmail.com>
2023-01-27 11:21:38 +0300

    Trigger for notifications

--- src/main/java/mix/projetcloudenchere/controllerMobile/TriggerController.java
+++ src/main/java/mix/projetcloudenchere/controllerMobile/TriggerController.java
@@ -2,6 +2,8 @@ package mix.projetcloudenchere.controllerMobile;
 
 
 import mix.projetcloudenchere.dao.DateDao;
+import mix.projetcloudenchere.model.Notification;
+import mix.projetcloudenchere.repository.NotificationRepository;
 import mix.projetcloudenchere.views.DetailEnchere;
 import mix.projetcloudenchere.viewsRepository.DetailEnchereRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,18 +26,38 @@ public class TriggerController {
     @Autowired
     DetailEnchereRepository detailEnchereRepository;
 
+    @Autowired
+    NotificationRepository notificationRepository;
+
     @GetMapping("/notifs")
     public ResponseEntity<?> checkNotif(){
+        try {
         List<DetailEnchere> unchecked = detailEnchereRepository.findAllUnchecked();
-        System.out.println("------find all unchecked : number " + unchecked.size() + "--------------");
-        for (DetailEnchere d : unchecked){
-            if (d.getDatefin().compareTo(new DateDao().getNow()) > 0) {
-                // La date de fin est après la date actuelle
+        List<Notification> newNotifs = new ArrayList<>();
+        if (unchecked.size()== 0){
+            return  new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            for (DetailEnchere d : unchecked) {
+                System.out.println("------find all unchecked : number " + unchecked.size() + "--------------");
+                if (d.getDatefin().compareTo(new DateDao().getNow()) < 0) {
+                    // La date de fin est après la date actuelle
+                    Notification notif = new Notification(d.getIdenchere(), d.getIdutilisateur());
+                    notificationRepository.save(notif);
+                    System.out.println("------------new Notification inserted ----------------");
+                }
+                else {
+                    System.out.println("else condition");
+                }
+
             }
-            System.out.println("ok");
+            return new ResponseEntity<>(HttpStatus.OK);
         }
 
-
-        return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 }
